e896372cb2bef3d3afad88c7c275c895925dc761
Airon Herbert <aironherbertms@gmail.com>
2023-08-30 16:22:52 -0300

    fix: responsividade do modal

--- src/components/card-selection.tsx
+++ src/components/card-selection.tsx
@@ -10,8 +10,10 @@ import {
   TableRow,
 } from "@mui/material";
 import { NIPES, VALUES } from "../App";
+import styled from "@emotion/styled";
 
 const style = {
+  boxSizing: "border-box",
   position: "absolute" as "absolute",
   top: "50%",
   left: "50%",
@@ -20,9 +22,15 @@ const style = {
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  maxHeight: "80vh",
-  p: 4,
+  maxWidth: "90vw",
+  padding: "10px 0",
 };
+
+const CustomTable = styled(Table)`
+  & tr td {
+    padding: 0;
+  }
+`;
 
 interface Proptypes {
   onClose: () => void;
@@ -34,41 +42,39 @@ export default function CardSelection({ onClose, onChoose }: Proptypes) {
     <Modal open onClose={onClose}>
       <Box sx={style}>
         <h2 style={{ margin: 0, textAlign: "center" }}>Selecione a carta</h2>
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          <TableContainer sx={{ maxHeight: 440 }}>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Cartas</TableCell>
+        <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+          <CustomTable stickyHeader size="small" style={{ maxWidth: "100%" }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Cartas</TableCell>
+                <TableCell align="center">
+                  <h1 style={{ color: "red" }}>♦</h1>
+                </TableCell>
+                <TableCell align="center">
+                  <h1>♣</h1>
+                </TableCell>
+                <TableCell align="center">
+                  <h1 style={{ color: "red" }}>♥</h1>
+                </TableCell>
+                <TableCell align="center">
+                  <h1>♠</h1>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            {VALUES.map((value) => (
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  <strong>{value}</strong>
+                </TableCell>
+                {NIPES.map((nipe) => (
                   <TableCell align="center">
-                    <h1 style={{ color: "red" }}>♦</h1>
+                    <Checkbox onClick={() => onChoose(value, nipe)} />
                   </TableCell>
-                  <TableCell align="center">
-                    <h1>♣</h1>
-                  </TableCell>
-                  <TableCell align="center">
-                    <h1 style={{ color: "red" }}>♥</h1>
-                  </TableCell>
-                  <TableCell align="center">
-                    <h1>♠</h1>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              {VALUES.map((value) => (
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    <strong>{value}</strong>
-                  </TableCell>
-                  {NIPES.map((nipe) => (
-                    <TableCell align="center">
-                      <Checkbox onClick={() => onChoose(value, nipe)} />
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </Table>
-          </TableContainer>
-        </Paper>
+                ))}
+              </TableRow>
+            ))}
+          </CustomTable>
+        </TableContainer>
       </Box>
     </Modal>
   );
